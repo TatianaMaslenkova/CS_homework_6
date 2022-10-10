7d57b95d5859212f1cda30c5a2021b63ae913f26
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-10-10 19:12:24 +0300

    Task 41 added

---
+++ Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿//Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ ENTER. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 5 0 7 8 - 2 - 2-> 2
+
+Console.Clear();
+Console.Write("Введите число: ");
+int quantity = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Введите {quantity} чисел: ");
+double[] array = new double[quantity];
+double result = PositiveNumCounter(array);
+Console.WriteLine($"Количество чисел больше 0, которые ввёл пользователь: {result}");
+
+int PositiveNumCounter(double[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = double.Parse(Console.ReadLine()!);
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
